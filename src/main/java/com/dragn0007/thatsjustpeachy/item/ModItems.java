82c695fa3a3a770c47859802b6e406208dd4887a
DragN0007 <cooper.kylie@gmail.com>
2023-09-10 22:48:03 -0500

    Init

---
+++ src/main/java/com/dragn0007/thatsjustpeachy/item/ModItems.java
@@ -0,0 +1,37 @@
+package com.dragn0007.thatsjustpeachy.item;
+
+
+import com.dragn0007.thatsjustpeachy.JustPeachy;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, JustPeachy.MODID);
+
+
+    //Food
+    public static final RegistryObject<Item> PEACH = ITEMS.register("peach",
+            () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(4).saturationMod(1).build()).tab(ModItemGroup.GROUP)));
+
+    //Drinks
+    public static final RegistryObject<Item> PEACH_TEA = ITEMS.register("peach_tea",
+            () -> new CustomDrinkItem(
+                    new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 2880, 3,true, false),
+                    new MobEffectInstance(MobEffects.REGENERATION, 2880, 1, true, false)
+            ));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
